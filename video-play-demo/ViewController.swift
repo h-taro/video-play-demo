85378a54e8ad698573d3ba27050acab6574b3bd0
h-taro <t.hiraishi@bravesoft.co.jp>
2022-12-26 23:55:51 +0900

    Initial Commit

---
+++ video-play-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video-play-demo
+//
+//  Created by 平石　太郎 on 2022/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
